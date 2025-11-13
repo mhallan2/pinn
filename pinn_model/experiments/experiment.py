f4f5a05647d1e58e5f109d0359e5106088b0930a
mhallan2 <vishnyakovgg@gmail.com>
2025-11-13 09:30:35 +0300

    Jupyter notebook

--- pinn_model/experiments/experiment.py
+++ pinn_model/experiments/experiment.py
@@ -1,11 +1,12 @@
 from abc import ABC, abstractmethod
+from dataclasses import replace
 from utils import compute_errors
 from pde import u_exact
-
+from experiments.result import LambdaRunResult
 
 
 class Experiment(ABC):
-    """Абстрактный базовый класс для всех экспериментов."""
+    """Абстрактный базовый класс для экспериментов с гиперпараметрами."""
     
     def __init__(self, cfg, model_cls, f_func, g_func, data_gen):
         self.cfg = cfg
@@ -57,4 +58,31 @@ class Experiment(ABC):
         print(f"{self.get_experiment_name()} | RMSE={rmse_final:.3e} | Rel L2={rel_l2_final:.3e}")
 
         return self.create_result(history, rmse_final, rel_l2_final)
-    
+
+class LambdaExperiment(Experiment):
+    """
+    Эксперимент по изучению влияния весового коэффициента λ
+    перед слагаемым с граничными условиями в функции потерь
+    на точность и эффективность решения.
+    """
+
+    def __init__(self, lam_bc, cfg, model_cls, f_func, g_func, data_gen):
+        super().__init__(cfg, model_cls, f_func, g_func, data_gen)
+        self.lam_bc = lam_bc
+
+    def get_experiment_name(self):
+        return f"LambdaExperiment: λ = {self.lam_bc}"
+
+    def prepare_training_config(self):
+        return replace(self.cfg, lam_bc=self.lam_bc)
+
+    def create_result(self, history, rmse_final, rel_l2_final):
+        """Создает объект результата"""
+        return LambdaRunResult(
+            lam_bc=self.lam_bc,
+            history=history,
+            rmse_final=rmse_final,
+            rel_l2_final=rel_l2_final,
+            rmse_per_epoch=self.rmse_per_epoch,
+            rel_l2_per_epoch=self.rel_l2_per_epoch,
+        )
